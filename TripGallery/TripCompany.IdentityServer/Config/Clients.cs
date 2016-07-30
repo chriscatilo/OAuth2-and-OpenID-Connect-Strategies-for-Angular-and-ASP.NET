40af6ad5991b6853d1f8cc7a52f71e178f0656e0
Christopher Catilo <chriscatilo@hotmail.com>
2016-07-30 21:28:40 +0100

    Client Credential Flow 1. Added an OAuth client for Trip Gallery MVC client to the Identity Server 2. MVCClient: install-package IdentityModel 3. From MVCClient,  request the access token containing the gallerymanagement scope

--- TripGallery/TripCompany.IdentityServer/Config/Clients.cs
+++ TripGallery/TripCompany.IdentityServer/Config/Clients.cs
@@ -9,8 +9,64 @@ namespace TripCompany.IdentityServer.Config
     public static class Clients
     {
         public static IEnumerable<Client> Get()
-        { 
-            return new List<Client>(); 
+        {
+            return new[]
+            {
+                CreateTripGalleryClientRegistration()
+            };
+        }
+
+
+        /// <summary>
+        /// Create an OAuth client for Trip Gallery
+        /// </summary>
+        private static Client CreateTripGalleryClientRegistration()
+        {
+            // define the client and from the application that requires an access token to access an API,
+            // we defer to that client and trigger the authorisation flow (or grant)
+
+
+            /* 
+             * This OAuth client will use Client Credentials
+             * Username and password is not passed.
+             * Instead a client Id and Client secret is exchanged for an access token.
+             * This method is suitable for Machine To Machine communication scenario
+             * And should only be used between non-public, confidential clients
+             * as a public client cannot safely store the client secret
+             * 
+             * 
+             *  Client                  Authorisation Server
+             *  ------                  --------------------
+             *      client id & secret
+             *      -------------------------->|
+             *      
+             *                      access token
+             *      |<--------------------------
+             */
+            var grant = new
+            {
+                ClientId = "tripgalleryclientcredentials",
+                ClientName = "Trip Gallery (Client Credentials)",
+                Flow = Flows.ClientCredentials
+            };
+
+            return new Client
+            {
+                ClientId = grant.ClientId,
+                ClientName = grant.ClientName,
+                Flow = grant.Flow,
+
+                ClientSecrets = new List<Secret>()
+                {
+                    new Secret(TripGallery.Constants.TripGalleryClientSecret.Sha256())
+                },
+
+                // allow all scopes to the client
+                AllowAccessToAllScopes = true,
+
+                // Optionally, We could have specified a list a scopes allowed for this client
+                // AllowedScopes = new List<string>() { "gallerymanagement" }
+            };
         }
     }
 }
--- TripGallery/TripGallery.MVCClient/Helpers/TripGalleryHttpClient.cs
+++ TripGallery/TripGallery.MVCClient/Helpers/TripGalleryHttpClient.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using IdentityModel.Client;
+using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web;
 
 namespace TripGallery.MVCClient.Helpers
@@ -15,7 +11,11 @@ namespace TripGallery.MVCClient.Helpers
 
         public static HttpClient GetClient()
         { 
-            HttpClient client = new HttpClient(); 
+            HttpClient client = new HttpClient();
+
+            var accessToken = GetAccessTokenAuthorizationCode();
+
+            client.SetBearerToken(accessToken);
            
             client.BaseAddress = new Uri(Constants.TripGalleryAPI);
 
@@ -25,6 +25,48 @@ namespace TripGallery.MVCClient.Helpers
 
             return client;
         }
- 
+
+
+        /// <summary>
+        /// Request the access token containing the gallerymanagement scope 
+        /// from the /token endpoint of the identiry server
+        /// passing down the client ID and client secret
+        /// </summary>
+        private static string RequestAccessTokenAuthorizationCode()
+        {
+            // create a token client
+            var tokenClient = new TokenClient
+                (
+                address: TripGallery.Constants.TripGallerySTSTokenEndpoint,
+                clientId: "tripgalleryclientcredentials",
+                clientSecret: TripGallery.Constants.TripGalleryClientSecret
+                );
+
+            // ask for a token, containing the gallerymanagement scope
+            var tokenResponse = tokenClient.RequestClientCredentialsAsync("gallerymanagement").Result;
+
+            // decode & write out the token so we can see what's in it
+            TokenHelper.DecodeAndWrite(tokenResponse.AccessToken);
+
+
+            return tokenResponse.AccessToken;
+        }
+
+        /// <summary>
+        /// Put the access token to the cookie
+        /// </summary>
+        /// <returns></returns>
+        private static string GetAccessTokenAuthorizationCode()
+        {
+            var accessToken = HttpContext.Current.Request.Cookies.Get("TripGalleryCookie")?["access_token"];
+
+            if (accessToken != null) return accessToken;
+
+            accessToken = RequestAccessTokenAuthorizationCode();
+
+            HttpContext.Current.Response.Cookies["TripGalleryCookie"]["access_token"] = accessToken;
+
+            return accessToken;
+        }
     }
 }
